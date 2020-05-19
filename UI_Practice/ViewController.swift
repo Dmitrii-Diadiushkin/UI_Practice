2485a1df12f922f876ad99fb0c926ec41df45a03
Dmitrii Diadiushkin <stephandvd@yandex.ru>
2020-05-19 19:33:21 +0500

    Initial Commit

---
+++ UI_Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI_Practice
+//
+//  Created by Dmitrii Diadiushkin on 19.05.2020.
+//  Copyright © 2020 Dmitrii Diadiushkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
