144c7ac89787e4b0838eca5e90ff4060661b3328
Dmitrii Diadiushkin <stephandvd@yandex.ru>
2020-06-02 19:13:15 +0500

    End of homework 6

---
+++ UI_Practice/Model/News.swift
@@ -0,0 +1,9 @@
+//
+//  News.swift
+//  UI_Practice
+//
+//  Created by Dmitrii Diadiushkin on 01.06.2020.
+//  Copyright © 2020 Dmitrii Diadiushkin. All rights reserved.
+//
+
+import Foundation
---
+++ UI_Practice/News/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  UI_Practice
+//
+//  Created by Dmitrii Diadiushkin on 01.06.2020.
+//  Copyright © 2020 Dmitrii Diadiushkin. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
